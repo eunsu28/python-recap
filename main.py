feb64afdfc538fbc21d1e3a1422a5c38f8a82833
AY_28 <77998267+eunsu28@users.noreply.github.com>
2022-12-24 05:01:16 +0000

    while statement

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 print("Hello World")
 
-for_i = 1
-if for_i == 1:
+for_if = 1
+if for_if == 1:
   print("2022 is over")
+
+for_while = 0
+while for_while < 10:
+  for_while = for_while + 1
+  print(for_while)
