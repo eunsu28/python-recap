e31b8e1b9628221617e23d3cecefe210da83a808
AY_28 <77998267+eunsu28@users.noreply.github.com>
2023-01-05 02:59:31 +0000

    clean up

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-print("Hello World")
-
-for_if = 1
-if for_if == 1:
-  print("2022 is over")
-
 for_while = 0
 while for_while < 10:
   for_while = for_while + 1
@@ -24,3 +18,4 @@ for i in range(1, 11):
 fighterjets = ["f117", "f22", "f18", "f16"]
 for i in fighterjets:
   print(i)
+
